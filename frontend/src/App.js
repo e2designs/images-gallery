c5120853c3afa3b370b808cd2eb64435bdbad447
Eric Evans <evans2designs@gmail.com>
2022-11-10 14:54:15 -0700

    Call to random photo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header'
 import Search from './components/Search'
 
-
-
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+const url = 'https://api.unsplash.com'
 const App = () => {
   const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
+    fetch(`${url}/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }
 
   return (
