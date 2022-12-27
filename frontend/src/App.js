0a461045d6cac98e74d6df45c149dd02bf07e4a6
Eric Evans <evans2designs@gmail.com>
2022-12-27 09:34:37 -0700

    prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header'
-import Search from './components/Search'
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
-const url = 'https://api.unsplash.com'
+const url = "https://api.unsplash.com";
 const App = () => {
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState("");
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -15,20 +15,19 @@ const App = () => {
       .then((result) => result.json())
       .then((data) => {
         console.log(data);
-
       })
       .catch((err) => {
         console.log(err);
-      })
-    setWord('');
-  }
+      });
+    setWord("");
+  };
 
   return (
     <div className="App">
-    <Header title="Images Gallery"/>
-    <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
