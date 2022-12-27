b5bcf77bc2453aa1482cb06e77f547ed537ac542
Eric Evans <evans2designs@gmail.com>
2022-12-27 12:08:16 -0700

    ImageCard with props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,13 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 const url = 'https://api.unsplash.com';
 const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
-  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -16,7 +16,7 @@ const App = () => {
     fetch(`${url}/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
       .then((result) => result.json())
       .then((data) => {
-        setImages([data, ...images]);
+        setImages([{ ...data, title: word }, ...images]);
       })
       .catch((err) => {
         console.log(err);
@@ -28,6 +28,7 @@ const App = () => {
     <div className="App">
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      {!!images.length && <ImageCard image={images[0]} />}
     </div>
   );
 };
